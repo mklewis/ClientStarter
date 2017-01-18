4ea9517c72b7951ccc6625569ce346b0bfc2c807
Michael Lewis <michael.k.lewis@gmail.com>
2017-01-18 16:18:55 -0600

    moving template out to separate file.

--- app/scripts/ngtable/ng-detailrow-directive.js
+++ app/scripts/ngtable/ng-detailrow-directive.js
@@ -3,7 +3,7 @@
 angular.module('clientApp').directive("detailRow", function(){
     return {
         restirct:"E",
-        template: '<td colspan="{{tableCtrl[headings].length}}"><table class="table table-striped table-condensed"><tr ng-repeat="h in tableCtrl[headings]" ng-show="tableCtrl[showColumnFn](h.field, true)"><td class="col-xs-4"> {{h.title}}</td><td class="col-xs-8 text-left">{{itemObject[h.field]}}</td></tr></table></td>',
+        templateUrl: "scripts/ngtable/detail-row.html",
         replace: false,
         scope: {
             itemObject: "=",  //item object for each row.
