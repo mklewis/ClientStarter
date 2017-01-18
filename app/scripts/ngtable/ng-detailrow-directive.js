3c36f56e800b809a31e8b0677b488106282196b8
Michael Lewis <michael.k.lewis@gmail.com>
2017-01-18 16:11:36 -0600

    provided properties for controller heading property and show column function can be passed to directive, removing hard coding names.

--- app/scripts/ngtable/ng-detailrow-directive.js
+++ app/scripts/ngtable/ng-detailrow-directive.js
@@ -3,11 +3,13 @@
 angular.module('clientApp').directive("detailRow", function(){
     return {
         restirct:"E",
-        templateUrl: "scripts/ngtable/detail-row.html",
+        template: '<td colspan="{{tableCtrl[headings].length}}"><table class="table table-striped table-condensed"><tr ng-repeat="h in tableCtrl[headings]" ng-show="tableCtrl[showColumnFn](h.field, true)"><td class="col-xs-4"> {{h.title}}</td><td class="col-xs-8 text-left">{{itemObject[h.field]}}</td></tr></table></td>',
         replace: false,
         scope: {
             itemObject: "=",  //item object for each row.
-            tableCtrl: "="   //access to the tables controller.
+            tableCtrl: "=",   //access to the tables controller.
+            headings: "@",
+            showColumnFn: "@"
         }
     }
 });
